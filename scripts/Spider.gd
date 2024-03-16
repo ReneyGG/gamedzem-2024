fb19d96be590299b13e476cde961138c88888b45
DamianAaron <143437858+DamianAaron@users.noreply.github.com>
2024-03-16 20:49:50 +0100

    Spooder 4
    Działa, Pająk Frank (tak ma na imię, to tutaj to dowód, don't @ me) idzie do góry

--- scripts/Spider.gd
+++ scripts/Spider.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @export var offset = Vector2(0, -800)
-@export var duration = 7.5
-@export var rotation_speed = 1.5
+@export var duration = 20
+@export var mov_speed = 5
+
+var movinghead = 100
 
 func _ready():
 	start_tween()
@@ -11,12 +13,12 @@ func _ready():
 #Poruszanie Kolizji
 func start_tween():
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	tween.tween_property($Area2D/CollisionShape2D, "position", Vector2(0,0), duration * 5)
+	tween.tween_property($Area2D, "position", offset, duration * mov_speed)
 
 #Poruszanie Ciała(Sprite)
 func start_tween2():
 	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	tween.tween_property($SpiderBody, "position", Vector2(0,0), duration * 5)
+	tween.tween_property($SpiderBody, "position", offset, duration * mov_speed)
 
 #DeathZone
 func _on_area_2d_body_entered(body):
